69e62afe1780c1f33c3d261cb9a7920367bfbd0b
Marotta <rafaelmarottag@gmail.com>
2025-06-15 18:47:47 -0300

    fix(api): update metrics API URL to use environment variable
    - Changed the metrics API URL in the GET handler to utilize the INTERNAL_API_URL environment variable for improved configuration and flexibility.

--- src/app/api/metrics/route.js
+++ src/app/api/metrics/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await fetch('http://localhost:8000/videos/metrics', {
+    const response = await fetch(`${INTERNAL_API_URL}/videos/metrics`, {
       headers: {
         'Content-Type': 'application/json',
       },
